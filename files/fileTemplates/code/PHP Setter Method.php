b0bedfcd27063a4b73c62e7e29e8e935b0771bee
Ashoka de Wit <sjokki.dewit@mediact.nl>
2018-01-24 10:59:06 +0800

    Use void return type for non fluent setter methods.
    PHP7.1 supports the void return type.

--- files/fileTemplates/code/PHP Setter Method.php
+++ files/fileTemplates/code/PHP Setter Method.php
@@ -3,7 +3,7 @@
  * 
  * @return void
  */
-public ${STATIC} function set${NAME}(#if (${SCALAR_TYPE_HINT})${SCALAR_TYPE_HINT} #else#end$${PARAM_NAME})
+public ${STATIC} function set${NAME}(#if (${SCALAR_TYPE_HINT})${SCALAR_TYPE_HINT} #else#end$${PARAM_NAME}): void
 {
 #if (${STATIC} == "static")
     self::$${FIELD_NAME} = $${PARAM_NAME};
